1666e56cf8cd4336a72a42317c62a0e2445392fd
happyeggchen <38799701+happyeggchen@users.noreply.github.com>
2021-03-28 00:27:08 +0800

    Enabled lfs

--- core.fish
+++ core.fish
@@ -40,7 +40,7 @@
   function wallpaper_list
     echo "---------online-----------"
       echo 'last update ->' 
-       curl https://raw.githubusercontent.com/happyeggchen/Distro_wallpapers/testing/raw/latest/date.txt
+       curl https://media.githubusercontent.com/media/happyeggchen/Distro_wallpapers/testing/raw/latest/date.txt
        echo "Available distros' wallpapers"
           echo "1-Debian | 2-Pop_OS!"
           echo "3-Zorin-OS | 4-aosp"
