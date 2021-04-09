2b70234446d08fe2b647b5f33a803f8fc16f221a
happyeggchen <happyegg.chen@hotmail.com>
2021-04-09 19:14:00 +0800

    modified:   core.fish

--- core.fish
+++ core.fish
@@ -32,8 +32,23 @@ function downloader
   end
 
 function self_installer
-      sudo cp core.fish /usr/bin/distro_wallpapers
+  if test -e /usr/bin/distro_wallpapers
+    echo -e "\033[0;31mfound a old file "/usr/bin/distro_wallpapers",delete it?It may not the old DWF so take care\033[0m"
+    echo -e "\033[0;31mtype y to remove,n to keep it\033[0m"
+    read -P "DWF-INSTALLER>" installer_found_old_one
+      switch $installer_found_old_one
+      case y
+      sudo rm /usr/bin/distro_wallpapers
+      echo "OK-REINSTALL IT AGAIN"
+      case n
+      sudo cp core.fish /usr/bin/distro_wallpapers_fish
+      case "*"
+        echo "unexcpet input"
+      end
+    else
+    sudo cp core.fish /usr/bin/distro_wallpapers_fish
   end
+end
 
 function installer
       if test -d ~/.cache/Distro_wallpapers-CLI-fish/$distro
@@ -47,7 +62,7 @@ function installer
   end
 
 function self_remover
-      sudo rm /usr/bin/distro_wallpapers
+      sudo rm /usr/bin/distro_wallpapers_fish
   end
 
 function remover
